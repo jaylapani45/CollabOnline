f5038d503b4f1349baa2b1aae8db562395f5f72a
jayLapani <jaylapani45@gmaill.com>
2024-02-17 17:00:09 +0530

    liveblock setup

--- collab/app/(dashboard)/_components/board-card/index.tsx
+++ collab/app/(dashboard)/_components/board-card/index.tsx
@@ -7,6 +7,8 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
 import { CopyIcon, Edit, MenuIcon, MoreHorizontalIcon } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 
 
 interface BoardCardProps {
@@ -27,10 +29,17 @@ export const BoardCard = ({id,orgId,title,authorId,authorName,imageUrl,time,isFa
     const createdAtLabel = formatDistanceToNow(time,{
         addSuffix:true
     })
+
+    const {pending,mutate} = useApiMutation(api.board.addFavorite);
+    const favorite = () =>{
+        console.log("clicked")
+        mutate({userId:userId,boardId:id,orgId:orgId})
+    }
     
     return(
-        <Link href={`/board/${id}`}>
         <div className="group aspect-[100/127] flex justify-between flex-col overflow-hidden rounded-md border border-black border-opacity-15">
+            <Link href={`/board/${id}`}>
+                <div className="group aspect-[100/127] flex justify-between flex-col overflow-hidden rounded-md border border-black border-opacity-15">
             
             <div className="relative flex-1 bg-amber-50">
             <Image src={imageUrl} alt={title} fill className="object-fit"/>
@@ -39,16 +48,11 @@ export const BoardCard = ({id,orgId,title,authorId,authorName,imageUrl,time,isFa
                 <button className="z-1 absolute top-3 right-3 opacity-0 group-hover:opacity-75 outline-none"><Edit className="stroke-1 h-5 w-5 opacity-75 hover:opacity-1000 hover:scale-125" /></button>
             </Actions>
             </div>
-            <Footer
-                isFavorite={isFavorite}
-                title={title}
-                authorLabel={authorLabel}
-                createdAtLabel={createdAtLabel}
-                onClick={()=>{}}
-                disabled={true}
-             />
-        </div>
+            </div>
+            
         </Link>
+        
+        </div>
     )
 }
 BoardCard.Skeleton = function BoardCardSkeleton(){
